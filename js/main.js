d7d076e00c9c9f0e6aa55511b35182e44f647de6
Fyranth <glebw@gmail.com>
2020-04-11 20:37:44 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+var slideIndex = 1;
+/*showDivs(slideIndex);*/
+
+function plusDivs(n, classname) {
+  showDivs(slideIndex += n, classname);
+}
+
+function showDivs(n, classname) {
+  var i;
+  var x = document.getElementsByClassName(classname);
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
